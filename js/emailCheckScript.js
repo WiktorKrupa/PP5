bba77250b9ab658befb72e422f3f135f90df1226
JuliaMekarska <s221455@student.uek.krakow.pl>
2024-01-18 00:55:19 +0100

    Front cd

--- js/emailCheckScript.js
+++ js/emailCheckScript.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const form = document.getElementById('emailToCheck');
+document.addEventListener("DOMContentLoaded", (event) => {
+  const form = document.getElementById("emailToCheck");
 
-    form.addEventListener('submit', function(e) {
-        e.preventDefault(); // Prevent the default form submission
-        const email = document.getElementById('email').value;
-        getUserReservations(email);
-    });
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission
+    const email = document.getElementById("email").value;
+    getUserReservations(email);
+  });
 });
 
 function getUserReservations(email) {
-    fetch(`http://localhost:3000/get-reservations?email=${email}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(reservations => {
-            localStorage.setItem('reservations', JSON.stringify(reservations));
-            window.location.href = '../userReservations.html'; // Redirect to the new page
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+  fetch(`http://localhost:3000/get-reservations?email=${email}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((reservations) => {
+      localStorage.setItem("reservations", JSON.stringify(reservations));
+      window.location.href = "../userReservations.html"; // Redirect to the new page
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
